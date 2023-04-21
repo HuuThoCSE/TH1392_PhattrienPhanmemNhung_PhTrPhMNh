9ae20ba0350cbf9312e9000f6826de583746f63a
Hữu Thọ CS <huutho.cs@gmail.com>
2023-04-21 17:23:31 +0700

    5h23pm21-04-2023

--- PhTrPhMNh_Week6_Thuchanh1/Week6_Thuchanh1_ModuleChucNang_Bai3/Week6_Thuchanh1_ModuleChucNang_Bai3.ino
+++ PhTrPhMNh_Week6_Thuchanh1/Week6_Thuchanh1_ModuleChucNang_Bai3/Week6_Thuchanh1_ModuleChucNang_Bai3.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+#include <DHT.h>
+#define DHTPIN A0
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define pinSOIL A0
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  dht.begin();
+  pinMode(pinSOIL, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float temp = dht.readTemperature();
+  float doam = analogRead(pinSOIL);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Nhiet do: ");
+  delay(temp);
+  lcd.setCursor(0,1);
+  lcd.print("Do am: ");
+  delay(500);
 }
